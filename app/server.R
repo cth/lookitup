d0de14cf816deaee00e3231a750d36d0c797b6c4
Christian Theil Have <christiantheilhave@gmail.com>
2014-02-19 10:49:44 +0100

    Exploratorium and input in same tab

--- app/server.R
+++ app/server.R
@@ -200,9 +200,7 @@ renderSummaryPanel <- function(input,session) {
 
 renderInputPanel <- function(input,session) {
 	print(input$input.list)
-	mainPanel(
-            h6("List of genes, SNPs (rsnumbers) and genomic ranges to by analysed"),
-            h6("(one per line)"),
+           span(h6("List of genes, SNPs (rsnumbers) and genomic ranges to by analysed (one per line)"),
             tags$textarea(id="input.list", rows="20", cols="60", "")
 	)
 }
--- app/ui.R
+++ app/ui.R
@@ -9,23 +9,26 @@ shinyUI(bootstrapPage(
     tags$head(
         tags$script(src = "js/session.js")
         ),
-    sidebarPanel(h4("Exploratorium"),
-        actionButton("lookupButton","Lookup"),
-        uiOutput("exploratorium.tab"),
-        actionButton("addRangeButton","Add Range")
-        ),
     mainPanel(
         tabsetPanel(id="top",
                     tabPanel("Session", 
                              mainPanel(
                                  wellPanel(
-				#p(uiOutput("session.status")),
                                      uiOutput("session.key"),
                                      actionButton("save.session", "Save session"),
                                      actionButton("restore.session","Restore session")),
                                  span(h3("Summary of selections:"),uiOutput("summary.tab")))
                              ),
-                    tabPanel("Input", uiOutput("input.tab")),
+
+                    tabPanel("Input", 
+						span(
+					  	  div(class="span4",
+					    	tags$form(class="well",
+					        actionButton("lookupButton","Lookup"),
+					        uiOutput("exploratorium.tab"),
+					        actionButton("addRangeButton","Add Range"))),
+					  	  div(class="span3",
+							uiOutput("input.tab")))),
                     tabPanel("Cohorts", uiOutput("cohorts.tab")),
                     tabPanel("Phenotypes", uiOutput("phenotypes.tab")),
                     tabPanel("Covariates", uiOutput("covariates.tab")),
